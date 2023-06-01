02de26b439327ded3c20931a74e5f63ee1a16273
SantiagoTangarife <80987278+SantiagoTangarife@users.noreply.github.com>
2023-06-01 18:57:36 -0500

    Update PacienteController.java

--- src/main/java/com/udea/ParcialEquipo1/Controller/PacienteController.java
+++ src/main/java/com/udea/ParcialEquipo1/Controller/PacienteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/historiaClinica")
+@RequestMapping("/pacientes")
 @CrossOrigin("*")
 public class PacienteController {
 
