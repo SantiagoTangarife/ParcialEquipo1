01b668e15ec6e7aae1d7389a571256a670ed151d
SantiagoTangarife <80987278+SantiagoTangarife@users.noreply.github.com>
2023-06-01 16:22:49 -0500

    Initial commit

---
+++ src/test/java/com/udea/ParcialEquipo1/ParcialEquipo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.ParcialEquipo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialEquipo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
